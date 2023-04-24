9dd7c8bbbf3686fbade0ef6f9a2574f756af8dfa
Lickton <lilihuanmao@gmail.com>
2023-04-24 19:06:34 +0800

    finish chat room

--- IRC/server/src/main.c
+++ IRC/server/src/main.c
@@ -2,294 +2,386 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <pthread.h>
+#include <time.h>
+#include <unistd.h>
+
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <pthread.h>
 #include <sys/socket.h>
-#include <unistd.h>
 
 #include "../include/log.h"
 
-/* User states */
-#define UNDEF       0           /* undefined */
-#define ONLINE      1           /* user connected with server */
-#define OFFLINE     2           /* user not connected with server */
-
 /* Misc manifest constants */
-#define MAX_BUFFER  1024        /* max buffer size */
-#define MAX_NAME    7           /* at most 7 characters */
-#define MAX_USERS   40          /* at most MAX_USERS instantaneous */
-#define PORT        55312       /* use this port to communicate */
+#define PORT 						55312
+#define MAX_USERS 			50
+#define MAX_NAME_LEM 		20
+#define MAX_MSG_LEN 		256
+#define MAX_MSG_NUM 		100
+#define MAX_BUFFER 			1024
 
-/* Global variables */
-int nextuid = 1;                /* next user ID to allocate */
-struct user_t {                 /* the user struct */
-  int   uid;                    /* user id */
-  pthread_t tid;                /* process ID */
-  char  *name;                  /* name showed to other users */
-  char  *ipv4_addr;             /* user's ipv4 address */
-  int   port;                   /* user's port */
-  int   fd;                     /* fd to receive and send msg */
+/* Global varibles begins */
+typedef enum {
+  REGISTE,
+  SEND_PUBLIC,
+  SEND_PRIVATE,
+  QUIT
+} command_option;
+
+typedef enum {
+  SESSION_ONE,
+  SESSION_TWO,
+  SESSION_THREE
+} screen_num;
+
+typedef struct message {
+  time_t timestamp;
+  int uid_from;
+  int uid_to;
+  char user_name[MAX_NAME_LEM];
+  char content[MAX_MSG_LEN];
+} message_t;
+
+struct message_encode {
+  screen_num screem;
+  struct message message_t;
 };
-struct user_t users[MAX_USERS]; /* The user list */
-pthread_mutex_t user_list_mutex = PTHREAD_MUTEX_INITIALIZER; /* user list mutex */
-/* End global variables */
+
+struct message_decode {
+  command_option opt;
+  struct message message_t;
+};
+
+typedef struct user {
+  int uid;
+  char user_name[MAX_NAME_LEM];
+  int sockfd;
+} user_t;
+
+typedef struct node {
+  user_t user;
+  struct node *next;
+} node_t;
+
+typedef struct queue {
+  node_t *front;
+  node_t *rear;
+  int size;
+} queue_t;
+queue_t *users;
+
+typedef struct message_queue {
+  struct message_decode *messages[MAX_MSG_NUM];
+  int front;
+  int rear;
+  int size;
+} message_queue_t;
+message_queue_t *messages;
+
+pthread_mutex_t users_mutex_lock;
+pthread_mutex_t message_mutex_lock;
+pthread_cond_t user_change_cond;
+
+int next_uid;
+/* Global varibles ends */
 
 /* Function prototypes */
-void initusers();
-void clearuser(struct user_t *user);
-int maxuid(struct user_t *users);
-int adduser(struct user_t *user);
-int deleteuser(struct user_t *user, pthread_t tid);
+void *handle_client(void *arg);
+void *send_msg();
+void *broadcast_users();
+void enqueue(queue_t *q, user_t user);
+void dequeue(queue_t *q, int uid);
+void enqueue_message(message_queue_t *q, struct message_decode *message);
+struct message_decode *dequeue_message(message_queue_t *q);
 
-int safe_listen (int __fd, int __n);
-int safe_socket(int __domain, int __type, int __protocol);
-void safe_bind(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len);
-int safe_accept (int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len);
-
-/* communication functions */
-void forward(char *__msg);
-void *request_hanlder(void *__user);
-
-/* main - The server main routine */
 int main(int argc, char *argv[])
 {
   log_message(LOG_INFO, "Server starts");
 
-  initusers();
+  int server_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if (server_fd < 0) {
+    log_message(LOG_ERROR, "create socket error");
+    return -1;
+  }
+  log_message(LOG_INFO, "Socket created");
 
-  int server_sockfd = safe_socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family 			= AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port 				= htons(PORT);
 
-  // bind socket
-  struct sockaddr_in servAddr;
-  bzero(&servAddr, sizeof(servAddr));
-  servAddr.sin_family				= AF_INET;
-  servAddr.sin_addr.s_addr 	= htonl(INADDR_ANY);
-  servAddr.sin_port         = htons(PORT);
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    log_message(LOG_ERROR, "Bind error");
+    return -1;
+  }
+  log_message(LOG_INFO, "Bind port:55312");
 
-  safe_bind(server_sockfd, (struct sockaddr*)&servAddr, sizeof(servAddr));
+  if (listen(server_fd, MAX_USERS) == -1) {
+    log_message(LOG_ERROR, "Listen failed");
+    return -1;
+  }
+  log_message(LOG_INFO, "Listen state");
 
-  // convert state to listen state
-  safe_listen(server_sockfd, MAX_USERS);
+  users = (queue_t*)malloc(sizeof(queue_t));
+  users->size = 0;
+  users->front = users->rear = NULL;
 
-  // wait for connection
+  messages = (message_queue_t*)malloc(sizeof(message_queue_t));
+  messages->front = messages->rear = messages->size = 0;
+
+  pthread_mutex_init(&users_mutex_lock, NULL);
+  pthread_mutex_init(&message_mutex_lock, NULL);
+  pthread_cond_init(&user_change_cond, NULL);
+
+  pthread_t comsumer_tid, broadcast_tid;
+  pthread_create(&comsumer_tid, NULL, send_msg, NULL);
+  pthread_create(&broadcast_tid, NULL, broadcast_users, NULL);
+
   struct sockaddr_in client_addr;
-  socklen_t client_addr_len;
+  socklen_t len;
 
   while (1) {
-    int client_socket = safe_accept(
-        server_sockfd, (struct sockaddr*)&client_addr,
-        &client_addr_len);
-    char buff[MAX_BUFFER];
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
+    if (client_fd == -1) {
+      log_message(LOG_ERROR, "Accept failed");
+      exit(EXIT_FAILURE);
+    }
 
-    pthread_t th;
-    struct user_t user;
-    user.fd = client_socket;
-    user.ipv4_addr = (char *)inet_ntop(AF_INET, &client_addr.sin_addr, buff, sizeof(buff));
-    user.port = ntohs(client_addr.sin_port);
-    user.name = "test";
-    user.tid = *(&th);
-    log_message(LOG_DEBUG, "User info created");
+    log_message(LOG_INFO, "Accepted");
 
-    pthread_create(&th, NULL, request_hanlder, (void *)&user);
+    pthread_t tid;
+    if (pthread_create(&tid, NULL, handle_client, (void *)&client_fd) != 0) {
+      log_message(LOG_ERROR, "Create thread failed");
+      exit(EXIT_FAILURE);
+    }
   }
 
-  log_message(LOG_DEBUG, "Server closed");
-  close(server_sockfd);
+  close(server_fd);
   return EXIT_SUCCESS;
 }
 
-/**********************************
- * Socket communication functions
- *********************************/
+/*********************
+ * Pthread Funcitons
+ ********************/
 
-/* handle the request sent by user */
-void *request_hanlder(void *__user)
+void *handle_client(void *arg)
 {
-  log_message(LOG_DEBUG, "Come into request_hanlder");
-  struct user_t *user = (struct user_t *)__user;
-  char buff[MAX_BUFFER];
-  adduser(__user);
-  log_message(LOG_DEBUG, "Leave adduser");
+  int client_fd = *(int *)arg;
 
-  while (1) {
-    bzero(buff, sizeof(buff));
-    read(user->fd, buff, sizeof(buff));
-    buff[strlen(buff) - 1] = '\0';
-    log_message(LOG_INFO, "Receive message");
-    forward(buff);
+  user_t user;
+
+  int online = 1;
+  char info[MAX_BUFFER];
+
+  while (online) {
+    // log_message(LOG_DEBUG, "Still online");
+
+    struct message_decode msg;
+    bzero(info, sizeof(info));
+    read(client_fd, (void *)&msg, sizeof(msg));
+    // log_message(LOG_DEBUG, "received info");
+
+    switch (msg.opt) {
+      case REGISTE:
+        pthread_mutex_lock(&users_mutex_lock);
+
+        strcpy(user.user_name, msg.message_t.user_name);
+        user.sockfd = client_fd;
+        user.uid = ++next_uid;
+        write(client_fd, (void*)&user.uid, sizeof(user.uid));
+        enqueue(users, user);
+
+        pthread_mutex_unlock(&users_mutex_lock);
+        break;
+      case SEND_PRIVATE:
+      case SEND_PUBLIC:
+        pthread_mutex_lock(&message_mutex_lock);
+        
+    		enqueue_message(messages, &msg);
+
+        pthread_mutex_unlock(&message_mutex_lock);
+        break;
+     case QUIT:
+        pthread_mutex_lock(&users_mutex_lock);
+
+        dequeue(users, user.uid);
+
+        pthread_mutex_unlock(&users_mutex_lock);
+        sprintf(info, "User %d quit", user.uid);
+        log_message(LOG_INFO, info);
+        online = 0;
+        break;
+    }
   }
 
   return NULL;
 }
 
-/* send messages to all other users */
-void forward(char *__msg)
+void *send_msg()
 {
-  char *msg = (char *)__msg;
-
-  pthread_mutex_lock(&user_list_mutex);
-  for (int i = 0; i < MAX_USERS; i++) {
-    if (users[i].fd != -1) {
-      write(users[i].fd, msg, MAX_BUFFER);
-      char info[1024];
-      sprintf(info, "sending message to user:%d, \"%s\"", users[i].uid, msg);
-      log_message(LOG_DEBUG, info);
+  while (1) {
+    while (messages->size == 0) {
+      usleep(50);
     }
-  }
-  pthread_mutex_unlock(&user_list_mutex);
-}
 
-/******************************************
- * Routines that manipulate the user list
- *****************************************/
+    pthread_mutex_lock(&message_mutex_lock);
 
-/* initsuers - Initialize the user list */
-void initusers()
-{
-  for (int i = 0; i < MAX_USERS; i++) {
-    users[i].uid        = 0;
-    users[i].tid        = 0;
-    users[i].name       = "\0";
-    users[i].ipv4_addr  = "\0";
-    users[i].port       = 0;
-    users[i].fd         = -1;
-  }
+    struct message_decode *_msg = dequeue_message(messages);
+    struct message_encode to_be_sent = {SESSION_ONE, _msg->message_t};
 
-  log_message(LOG_INFO, "User list initialized");
-}
+    if (_msg->opt == SEND_PRIVATE) {
+      node_t *cur = users->front;
+      while (cur != NULL) {
+        if (cur->user.uid == _msg->message_t.uid_to)
+          break;
+        cur = cur->next;
+      }
 
-/* clearuser - Clear the entries in a job struct */
-void clearuser(struct user_t *user)
-{
-  char clearinfo[MAX_BUFFER];
-  sprintf(clearinfo, "User from %s:%d canceled. uid:%d", user->ipv4_addr, user->port, user->uid);
-  log_message(LOG_INFO, clearinfo);
-
-  user->uid = 0;
-  user->name = "\0";
-  close(user->fd);
-  user->fd = -1;
-  user->port = 0;
-  user->ipv4_addr = "\0";
-  user->tid = 0;
-}
-
-/* maxuid - Returns largest allocated user ID */
-int maxuid(struct user_t *user)
-{
-  int max = 0;
-  pthread_mutex_lock(&user_list_mutex);
-  for (int i = 0; i < MAX_USERS; i++) {
-    if (users[i].uid > max)
-      max = users[i].uid;
-  }
-  pthread_mutex_unlock(&user_list_mutex);
-  return max;
-}
-
-/* adduser - Add a user to the user list */
-int adduser(struct user_t *user)
-{
-  for (int i = 0; i < MAX_USERS; i++) {
-    if (users[i].uid == 0) {
-      /* mutex lock */
-      log_message(LOG_DEBUG, "Wait for mutex");
-      pthread_mutex_lock(&user_list_mutex);
-      log_message(LOG_DEBUG, "Gain the mutex");
-      users[i].fd   = user->fd;
-      users[i].uid  = nextuid++;
-      users[i].tid  = user->tid;
-      users[i].port = user->port;
-      if (nextuid > MAX_USERS)
-        nextuid = 1;
-      users[i].name = user->name;
-      users[i].ipv4_addr = user->ipv4_addr;
-      log_message(LOG_DEBUG, "Finish copy of info");
-
-      char info[MAX_BUFFER];
-      sprintf(info, "User from %s:%d registered. tid:%lu", user->ipv4_addr, user->port, users[i].tid);
-
-      /* mutex unlock */
-      pthread_mutex_unlock(&user_list_mutex);
-      log_message(LOG_DEBUG, "Leave the mutex");
-
-      log_message(LOG_INFO, info);
-      return 0;
+      if (cur != NULL)
+        write(cur->user.sockfd, (void *)&to_be_sent, sizeof(to_be_sent));
+      else {
+        // TODO Tell the client that user not found
+      }
     }
-  }
 
-  log_message(LOG_WARNING, "Too much users right now");
-  return 1;
-}
-
-/* deleteuser - Delete a job from the user list */
-int deleteuser(struct user_t *user, pthread_t tid)
-{
-  for (int i = 0; i < MAX_USERS; i++) {
-    if (users[i].tid == tid) {
-      clearuser(&users[i]);
-      nextuid = maxuid(users) + 1;
-      return 1;
+    if (_msg->opt == SEND_PUBLIC) {
+      node_t *cur = users->front;
+      while (cur != NULL) {
+        write(cur->user.sockfd, (void *)&to_be_sent, sizeof(to_be_sent));
+        cur = cur->next;
+      }
     }
+
+    pthread_mutex_unlock(&message_mutex_lock);
   }
 
-  return 0;
-}
- 
-/***********************************************
- * Safe functions - encapsolution for functions
- **********************************************/
-
-int safe_socket (int __domain, int __type, int __protocol)
-{
-  int ret = socket(__domain, __type, __protocol);
-  if (ret < 0) {
-    log_message(LOG_ERROR, "Create socket failed");
-    printf("Create socket failed\n");
-
-    exit(EXIT_FAILURE);
-  } else
-    log_message(LOG_INFO, "Socket is created");
-
-  return ret;
+  return NULL;
 }
 
-void safe_bind(int __fd, __CONST_SOCKADDR_ARG __addr, socklen_t __len)
+void *broadcast_users()
 {
-  int res = bind(__fd, __addr, __len);
-  if (res < 0) {
-    log_message(LOG_ERROR, "Bind failed");
-    printf("Bind failed\n");
+  while (1) {
+    char buff[MAX_BUFFER];
 
-    exit(EXIT_FAILURE);
-  } else
-    log_message(LOG_INFO, "Address is bound");
-}
+    pthread_mutex_lock(&users_mutex_lock);
+    pthread_cond_wait(&user_change_cond, &users_mutex_lock);
+    // log_message(LOG_DEBUG, "broadcast_users wake up");
 
-int safe_listen (int __fd, int __n)
-{
-  int ret = listen(__fd, __n);
-  if (ret < 0) {
-    log_message(LOG_ERROR, "Listen failed");
-    printf("Listen failed\n");
+    node_t *curr1 = users->front, *curr2 = users->front;
+    time_t currtime;
+    time(&currtime);
 
-    exit(EXIT_FAILURE);
-  } else
-    log_message(LOG_INFO, "Listen state");
+    while (curr1 != NULL) {
+      curr2 = users->front;
+      while (1) {
+       bzero(buff, sizeof(buff));
 
-  return ret;
-}
+       struct message_encode to_be_sent;
+       to_be_sent.screem = SESSION_THREE;
+       char userinfo[MAX_BUFFER];
 
-int safe_accept (int __fd, __SOCKADDR_ARG __addr, socklen_t *__restrict __addr_len)
-{
-  int ret = accept(__fd, __addr, __addr_len);
-  if (ret < 0) {
-    log_message(LOG_ERROR, "Accept client connection failed");
-    printf("Accept client connection failed\n");
+       if (curr2 == users->front) {
+         /* to_uid == from_uid == -1   represent for start of sending user list */
+         to_be_sent.message_t.uid_from = to_be_sent.message_t.uid_to = -1;
+       	 write(curr1->user.sockfd, (void *)&to_be_sent, sizeof(to_be_sent));
+       }
 
-    exit(EXIT_FAILURE);
+       if (curr2 != NULL) {
+	       sprintf(userinfo, "[%2d] %s", curr2->user.uid, curr2->user.user_name);
+         /* to_uid == from_uid == -2   represent for sending user list */
+         to_be_sent.message_t.uid_from = to_be_sent.message_t.uid_to = -2;
+         strcpy(to_be_sent.message_t.content, userinfo);
+       	 write(curr1->user.sockfd, (void *)&to_be_sent, sizeof(to_be_sent));
+       } else {
+         /* to_uid == from_uid == -3   represent for ending of sending user list */
+         to_be_sent.message_t.uid_from = to_be_sent.message_t.uid_to = -3;
+         write(curr1->user.sockfd, (void *)&to_be_sent, sizeof(to_be_sent));
+         break;
+       }
+
+       curr2 = curr2->next;
+      }
+      curr1 = curr1->next;
+    }
+
+    pthread_mutex_unlock(&users_mutex_lock);
   }
 
-  return ret;
+  return NULL;
+}
+
+/**********************************
+ * Functions manipulate the queue
+ *********************************/
+
+/* enqueue - add user to the user list */
+void enqueue(queue_t *q, user_t user)
+{
+  node_t *new_node = (node_t *)malloc(sizeof(node_t));
+  new_node->user = user;
+  new_node->next = NULL;
+
+  if (q->rear == NULL) {
+    q->front = new_node;
+    q->rear = new_node;
+  } else {
+    q->rear->next = new_node;
+    q->rear = new_node;
+  }
+
+  q->size++;
+  pthread_cond_signal(&user_change_cond);
+}
+
+/* dequeue - delete user from the user list */
+void dequeue(queue_t *q, int uid)
+{
+  node_t *prev = NULL;
+  node_t *curr = q->front;
+
+  while (curr != NULL) {
+    if (curr->user.uid == uid) {
+      if (prev == NULL) q->front = curr->next;
+      else prev->next = curr->next;
+
+      if (curr == q->rear)
+        q->rear = prev;
+
+      close(curr->user.sockfd);
+      free(curr);
+      q->size--;
+      break;
+    }
+    prev = curr;
+    curr = curr->next;
+  }
+  pthread_cond_signal(&user_change_cond);
+}
+
+/* enqueue_message - insert message to the message list */
+void enqueue_message(message_queue_t *q, struct message_decode *message)
+{
+  if (q->size == MAX_MSG_NUM) {
+    // log_message(LOG_DEBUG, "Too much messages");
+    return;
+  }
+
+  q->messages[q->front] = message;
+
+  q->rear = (q->rear + 1) % MAX_MSG_NUM;
+  q->size++;
+}
+
+/* dequeue_message - pop message from the message list */
+struct message_decode *dequeue_message(message_queue_t *q)
+{
+  if (q->size == 0) {
+    return NULL;
+  }
+
+  struct message_decode *message = q->messages[q->front];
+  q->front = (q->front + 1) % MAX_MSG_NUM;
+  q->size--;
+  return message;
 }
